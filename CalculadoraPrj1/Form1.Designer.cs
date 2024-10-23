b3f2a3f640ca82e757009a8a4b1bc51ce0ab0b9c
Cesar <cesar.santiago@utp.ac.pa>
2024-10-23 14:20:19 -0700

    Add project files.

---
+++ CalculadoraPrj1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace CalculadoraPrj1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnPunto = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnPosNeg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(216, 129);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(60, 60);
+            this.btnSumar.TabIndex = 1;
+            this.btnSumar.Text = "+";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(216, 195);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(60, 60);
+            this.btnRestar.TabIndex = 2;
+            this.btnRestar.Text = "-";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(216, 261);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(60, 60);
+            this.btnMultiplicar.TabIndex = 3;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(216, 327);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(60, 60);
+            this.btnDividir.TabIndex = 4;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(32, 327);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(152, 60);
+            this.btnIgual.TabIndex = 5;
+            this.btnIgual.Text = "= (Calcular)";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click_1);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(89, 269);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(40, 40);
+            this.btn0.TabIndex = 6;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(32, 221);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(40, 40);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(89, 221);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(40, 40);
+            this.btn2.TabIndex = 8;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(144, 221);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(40, 40);
+            this.btn3.TabIndex = 9;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(144, 175);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(40, 40);
+            this.btn6.TabIndex = 12;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(89, 175);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(40, 40);
+            this.btn5.TabIndex = 11;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(32, 175);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(40, 40);
+            this.btn4.TabIndex = 10;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(32, 129);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(40, 40);
+            this.btn7.TabIndex = 13;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(89, 129);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(40, 40);
+            this.btn8.TabIndex = 14;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(144, 129);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(40, 40);
+            this.btn9.TabIndex = 15;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnPunto
+            // 
+            this.btnPunto.Location = new System.Drawing.Point(144, 269);
+            this.btnPunto.Name = "btnPunto";
+            this.btnPunto.Size = new System.Drawing.Size(40, 40);
+            this.btnPunto.TabIndex = 16;
+            this.btnPunto.Text = ".";
+            this.btnPunto.UseVisualStyleBackColor = true;
+            this.btnPunto.Click += new System.EventHandler(this.btnPunto_Click_1);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(32, 63);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(152, 60);
+            this.btnLimpiar.TabIndex = 17;
+            this.btnLimpiar.Text = "C (Limpiar)";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click_1);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(200, 63);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(76, 60);
+            this.btnBorrar.TabIndex = 18;
+            this.btnBorrar.Text = "<= (Borrar numero)";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnPosNeg
+            // 
+            this.btnPosNeg.Location = new System.Drawing.Point(32, 269);
+            this.btnPosNeg.Name = "btnPosNeg";
+            this.btnPosNeg.Size = new System.Drawing.Size(40, 40);
+            this.btnPosNeg.TabIndex = 19;
+            this.btnPosNeg.Text = "+/-";
+            this.btnPosNeg.UseVisualStyleBackColor = true;
+            this.btnPosNeg.Click += new System.EventHandler(this.btnPosNeg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 399);
+            this.Controls.Add(this.btnPosNeg);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnPunto);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Calculadora!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnPunto;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnPosNeg;
+    }
+}
+
---
+++ CalculadoraPrj1/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CalculadoraPrj1
+{
+    public partial class Form1 : Form
+    {
+        // Proyecto 1
+        // Cesar Santiago
+        // Sara Lee
+
+        // Calculadora
+
+
+        // 1. Instanciamos variables a utilizar
+        double primerNumero = 0;
+        string operacion = "";
+        bool operacionSeleccionada = false;
+        private SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConectarBaseDeDatos();
+        }
+
+        private void ConectarBaseDeDatos()
+        {
+            try
+            {
+                // Cadena de conexión a la base de datos (ajusta el Data Source según tu configuración)
+                string connectionString = @"Server=.\SQLEXPRESS;Database=calculadora;Trusted_Connection=True;";
+                connection = new SqlConnection(connectionString);
+                connection.Open(); // Conexión abierta al iniciar el programa
+
+                // Mensaje opcional de éxito
+                MessageBox.Show("Conexión a la base de datos establecida.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                MessageBox.Show("Error al conectar con la base de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+            // 2. Creamos los siguientes metodos para usar en multiples botones
+            // a. Metodo AgregarNumero para que agregue numero al textbox cada vez que usamos un boton numerico
+            // b. Metodo EsNumeroValido para hacer un constraint que no permita ingresar valores no numericos
+            // c. Metodo SeleccionarOperacion para que realice la distincion entre el primer numero a calcular y el segundo antes y despues del operador
+            private void AgregarNumero(string numero)
+        {
+           
+            if (operacionSeleccionada)
+            {
+                textBox1.Clear();
+                operacionSeleccionada = false;
+            }
+
+            // Concatenamos el número en el TextBox
+            textBox1.Text += numero;
+        }
+
+        private bool EsNumeroValido(string texto)
+        {
+            double numero;
+            return double.TryParse(texto, out numero);
+        }
+
+        private void SeleccionarOperacion(string operacionSeleccionada)
+        {
+            if (EsNumeroValido(textBox1.Text))
+            {
+                primerNumero = Convert.ToDouble(textBox1.Text);
+                operacion = operacionSeleccionada;
+                this.operacionSeleccionada = true;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        // 3. Creamos los botones del 0 al 9 para ingresar numeros a la calculadora
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            // Obtiene el número del botón presionado
+            Button boton = (Button)sender;
+
+            // Llama al método para agregar el número
+            AgregarNumero(boton.Text);
+        }
+
+        // 4. Creamos un boton de Punto decimal para numeros decimales
+        private void btnPunto_Click_1(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains("."))
+            {
+                if (textBox1.Text == "")
+                {
+                    textBox1.Text = "0.";
+                }
+                else
+                {
+                    textBox1.Text += ".";
+                }
+            }
+        }
+
+        // 5. Creamos un boton de = para calcular resultado
+
+        private void btnIgual_Click_1(object sender, EventArgs e)
+        {
+            if (EsNumeroValido(textBox1.Text))
+            {
+                double segundoNumero = Convert.ToDouble(textBox1.Text);
+                double resultado = 0;
+
+                switch (operacion)
+                {
+                    case "+":
+                        resultado = primerNumero + segundoNumero;
+                        break;
+                    case "-":
+                        resultado = primerNumero - segundoNumero;
+                        break;
+                    case "*":
+                        resultado = primerNumero * segundoNumero;
+                        break;
+                    case "/":
+                        if (segundoNumero != 0)
+                            resultado = primerNumero / segundoNumero;
+                        else
+                            MessageBox.Show("Error: División por cero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+
+                textBox1.Text = resultado.ToString();
+
+                GuardarOperacionEnBaseDeDatos(primerNumero, segundoNumero, operacion, resultado);
+            
+        }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        // 6. Creamos un boton de limpiar la calculadora y anular operaciones realizadas
+        private void btnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            primerNumero = 0;
+            operacion = "";
+        }
+
+        // 7. Creamos un boton para borrar el ultimo numero ingresado
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }
+        }
+
+        // 8. Creamos un boton que cambie el signo de positivo a negativo y viceversa
+        private void btnPosNeg_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.StartsWith("-"))
+            {
+                textBox1.Text = textBox1.Text.Substring(1); // Remover el signo negativo
+            }
+            else
+            {
+                textBox1.Text = "-" + textBox1.Text; // Agregar el signo negativo
+            }
+        }
+
+
+        // 9. Creamos los botones de operaciones suma, resta, mult, div llamando el metodo de seleccionarOperacion
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+
+            Button boton = (Button)sender;
+            SeleccionarOperacion(boton.Text);
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            SeleccionarOperacion(boton.Text);
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            SeleccionarOperacion(boton.Text);
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            SeleccionarOperacion(boton.Text);
+        }
+
+        private void GuardarOperacionEnBaseDeDatos(double primerNumero, double segundoNumero, string operacion, double resultado)
+        {
+            try
+            {
+                // Comando SQL para insertar los valores en la tabla
+                string query = "INSERT INTO calculadora_log (primernumero, segundonumero, operador, resultado) VALUES (@primerNumero, @segundoNumero, @operacion, @resultado)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Asignar los parámetros con los valores calculados
+                    command.Parameters.AddWithValue("@primerNumero", primerNumero);
+                    command.Parameters.AddWithValue("@segundoNumero", segundoNumero);
+                    command.Parameters.AddWithValue("@operacion", operacion);
+                    command.Parameters.AddWithValue("@resultado", resultado);
+
+                    // Ejecutar el comando
+                    command.ExecuteNonQuery();
+                }
+
+                // Mensaje opcional de éxito
+                MessageBox.Show("Operación guardada correctamente en la base de datos.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                MessageBox.Show("Error al guardar la operación en la base de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
